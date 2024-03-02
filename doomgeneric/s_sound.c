082abd546507e975df5dc141d287581001e99f56
lipsanen <jukspaz@gmail.com>
2024-03-03 01:07:57 +0200

    Removed some unused sound stuff

--- doomgeneric/s_sound.c
+++ doomgeneric/s_sound.c
@@ -596,7 +596,7 @@ void S_ChangeMusic(struct doom_data_t_* doom, int musicnum, int looping)
     // The Doom IWAD file has two versions of the intro music: d_intro
     // and d_introa.  The latter is used for OPL playback.
 
-    if (musicnum == mus_intro && (snd_musicdevice == SNDDEVICE_ADLIB || snd_musicdevice == SNDDEVICE_SB))
+    if (musicnum == mus_intro)
     {
         musicnum = mus_introa;
     }
